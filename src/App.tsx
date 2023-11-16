1e7fd2f7c1d213792f37c99ffad4c2e395ffd072
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-16 15:34:54 +0100

    App TSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import './App.css'
-import Navbar from './Components/Navbar';
-import Home from './Components/Home';
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Navbar />
