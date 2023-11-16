2ff0a5d8f291ae353637aa9b69d35f97a0d8aef2
Alejandro Sanchez <sanchezgalejandroj@gmail.com>
2023-11-16 14:51:59 +0100

    feat: añadiendo el formulario en el home page y editando los estilos del mismo

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,16 @@
-import '../styles/home.css';
-import Education from '../assets/Education.png';
+import "../styles/home.css";
+import Education from "../assets/Education.png";
+import { Form } from "./Form";
 
 const Home = () => {
   return (
-    <div className='Home-container'>
-        <div className='home-elements'><img src={Education} alt='Edu' id='Education'/> </div>
+    <div className="home-container">
+      <div className="home-elements">
+        <img src={Education} alt="Edu" id="Education" />{" "}
+      </div>
+      <Form />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
