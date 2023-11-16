31ee098b11b96a4cc97aa83ca4d267cdb8f867ca
Alejandro Sanchez <sanchezgalejandroj@gmail.com>
2023-11-16 13:35:51 +0100

    feat: Inicializando el form

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../styles/home.css';
 import Education from '../assets/Education.png';
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState}from 'react'; 
+import {useState}from 'react'; 
 import '../styles/navbar.css';
 import {FaBars} from 'react-icons/fa6';
 import {Link} from 'react-scroll';
