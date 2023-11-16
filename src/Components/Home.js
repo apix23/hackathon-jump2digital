97888f128aaa8a72b90d4802f847f9ab0a42a43f
Sairazulfiqar <sairazulfiqaranjum@gmail.com>
2023-11-16 13:32:38 +0100

    new components

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/home.css';
+import Education from '../assets/Education.png';
+
+const Home = () => {
+  return (
+    <div className='Home-container'>
+        <div className='home-elements'><img src={Education} alt='Edu' id='Education'/> </div>
+    </div>
+  )
+}
+
+export default Home;
